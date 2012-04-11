c3085145320415098d0839f14073f130cccddfb0
Jeremy <Jeremy@JeremyMacBook.local>
2012-04-11 23:47:48 +0800

    New Files added

---
+++ Score Calculator/Player.h
@@ -0,0 +1,18 @@
+//
+//  Player.h
+//  Score Calculator
+//
+//  Created by Song Jeremy on 4/11/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+
+
+@interface Player : NSManagedObject
+
+@property (nonatomic, retain) NSString * name;
+@property (nonatomic, retain) NSDecimalNumber * score;
+
+@end
---
+++ Score Calculator/Player.m
@@ -0,0 +1,17 @@
+//
+//  Player.m
+//  Score Calculator
+//
+//  Created by Song Jeremy on 4/11/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "Player.h"
+
+
+@implementation Player
+
+@dynamic name;
+@dynamic score;
+
+@end
