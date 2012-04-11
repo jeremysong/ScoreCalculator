b8c2405ddb4317836a0d5ae97292024da283b64f
Jeremy <Jeremy@JeremyMacBook.local>
2012-04-11 23:52:37 +0800

    new files added

---
+++ Score Calculator/RootViewController.h
@@ -0,0 +1,13 @@
+//
+//  RootViewController.h
+//  Score Calculator
+//
+//  Created by Song Jeremy on 4/11/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UIViewController
+
+@end
---
+++ Score Calculator/RootViewController.m
@@ -0,0 +1,43 @@
+//
+//  RootViewController.m
+//  Score Calculator
+//
+//  Created by Song Jeremy on 4/11/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "RootViewController.h"
+
+@interface RootViewController ()
+
+@end
+
+@implementation RootViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
